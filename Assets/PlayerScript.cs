a16c8f19d1dcc5f01b0b464b6395873290201859
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2017-01-07 22:38:42 +0530

    Added sprites, animations and background scroller script

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	private Animator anim;
+	private Rigidbody2D rb;
+	public Vector2 jumpForce;
+
+	// Use this for initialization
+	void Start () {
+
+
+		anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			anim.SetInteger("State",1);
+			jump ();
+		}
+	}
+
+	void jump(){
+		rb.AddForce ( jumpForce ,ForceMode2D.Impulse);
+	}
+
+	void OnCollisionEnter2D(Collision2D target){
+		anim.SetInteger("State",0);
+	}
+}
---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour {
+
+	public float scrollSpeed;
+	private Vector2 savedOffset;
+	private Renderer r;
+	void Start () {
+		r = GetComponent<Renderer>();
+		savedOffset = r.sharedMaterial.GetTextureOffset ("_MainTex");
+	}
+
+	void Update () {
+		float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2 (x, savedOffset.y);
+		r.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
+
+	void OnDisable () {
+		r.sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+	}
+}
